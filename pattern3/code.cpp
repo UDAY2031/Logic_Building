be0ada671d295898de6798a9515e3630798cd263
UDAY KUMAR G <144527336+UDAY2031@users.noreply.github.com>
2024-10-22 18:05:47 +0530

    Create code.cpp

---
+++ pattern3/code.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+void pattern(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    int t;
+    cout<<"Enter the number of test cases"<<endl;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        int n;
+        cout<<"Enter the n value"<<endl;
+        cin>>n;
+        pattern(n);
+    }
+}
