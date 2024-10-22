213980dd280c9e9b189c9cbcf22ca2eb1a405b9c
UDAY KUMAR G <144527336+UDAY2031@users.noreply.github.com>
2024-10-22 18:41:03 +0530

    Update code.cpp

--- pattern7/code.cpp
+++ pattern7/code.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 void pattern(int n){
     for(int i=0;i<n;i++){
-        for(int j=0;j<n-i;j++){
+        for(int j=0;j<n-i-1;j++){
             cout<<" ";
         }
         for(int j=0;j<2*i+1;j++){
             cout<<"*";
         }
-        for(int j=0;j<n-i;j++){
+        for(int j=0;j<n-i-1;j++){
             cout<<" ";
         }
         cout<<endl;
