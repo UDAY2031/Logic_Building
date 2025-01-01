254971b7ba6b24b31394333509110ab3a5bc663e
UDAY KUMAR G <144527336+UDAY2031@users.noreply.github.com>
2025-01-01 20:08:18 +0530

    Create code.cpp

---
+++ pattern14/code.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+void pattern(int n){
+    for(int i=0;i<n;i++){
+        for(char j = 'A'; j <= 'A' + i;j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    int k;
+    cout<<"Enter the testcases:";
+    cin>>k;
+    for(int i=0;i<k;i++){
+        int n;
+        cin>>n;
+        pattern(n);
+    }
+}
